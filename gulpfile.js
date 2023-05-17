782d20ace8753b1f71322c6add76d300007b3100
drill-lancer <rickaddison7634@gmail.com>
2023-05-17 14:15:46 +0900

    fix: test

--- gulpfile.js
+++ gulpfile.js
@@ -7,41 +7,14 @@ var runSequence = require('run-sequence');
 gulp.task('dist', function() {
     return gulp.src(
             [
-							'./**/*.php',
-							'./**/*.txt',
-							'./**/*.css',
-							'./**/*.scss',
-							'./**/*.bat',
-							'./**/*.rb',
-							'./**/*.eot',
-							'./**/*.svg',
-							'./**/*.ttf',
-							'./**/*.woff',
-							'./**/*.woff2',
-							'./**/*.otf',
-							'./**/*.less',
-							'./**/*.png',
-							'./images/**',
 							'./inc/**',
-							'./assets/**',
 							'./admin/**',
-							'./languages/**',
-							"!./compile.bat",
-							"!./config.rb",
-							"!./tests/**",
-							"!./dist/**",
-							"!./node_modules/**"
+							'./vendor/**',
+							'./*.php',
+							'./*.txt',
+							'./*.png',
             ],
             { base: './' }
         )
         .pipe( gulp.dest( 'dist/vk-plugin-beta-tester' ) ); // distディレクトリに出力
 } );
-
-// replace_text_domain ////////////////////////////////////////////////
-gulp.task("replace_text_domain", function(done) {
-	// vk-admin
-	gulp.src(["./inc/vk-admin/package/*"])
-		.pipe(replace("vk_admin_textdomain","vk-plugin-beta-tester"))
-		.pipe(gulp.dest("./inc/vk-admin/package/"));
-	done();
-});
